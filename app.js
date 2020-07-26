6049def9f75ba778b200386abd687be8e4de98cc
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-26 15:52:45 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use((_req, _res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+
+  next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  const error = {
+    message: err.message,
+    status: err.status,
+    stack: err.stack,
+  };
+
+  res.status(err.status || 500);
+  res.json(error);
+});
+
+module.exports = app;
